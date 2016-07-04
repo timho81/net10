eb60922a74fa353d4d9ac9890a15062de2cb0de5
Tim Ho <timho/dung.hoviet@imipgroup.com>
2016-07-04 11:40:56 +0700

    Apply ACL on Job Req service

--- Server/api_app/endpoints/jobReqEP.js
+++ Server/api_app/endpoints/jobReqEP.js
@@ -11,6 +11,7 @@ var utils = require('../utils/utils.js');
 
 // Create a new req
 module.exports.create = function (req, res) {
+    
     if (!utils.isAuthorized(req, 'ROLE_MANAGER')) {
         console.log('You are unauthorized to create a new req');
         utils.sendJSONresponse(res, 403, {
